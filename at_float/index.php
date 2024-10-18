82cdbd85d0395f00768d9795706f301767437d2e
Joyuv <givanilsonmariz7@outlook.com>
2024-10-18 06:44:39 -0300

    At 11 & At float

---
+++ at_float/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div id="cabe">
+    <div id="bloco1"><p>bot_01</p></div>
+    <div id="bloco2"><p>bot_02</p></div>
+    <div id="bloco3"><p>bot_03</p></div>
+    <div id="bloco4"><p>bot_04</p></div>
+    </div>
+    <div id="corpo">
+        <div id="side">
+            <div class="texto">Texto_1</div>
+            <div class="texto">Texto_2</div>
+            <div class="texto">Texto_3</div>
+            <div class="texto">Texto_4</div>
+        </div>
+        <div float="right"id="conteudo"><h2>Página inicial</h2><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem nulla dolor corporis excepturi perspiciatis officia, rem, quas ex aliquid pariatur praesentium distinctio nisi aspernatur deserunt amet dignissimos sint? Voluptate, id! Lorem, ipsum dolor sit amet consectetur adipisicing elit. Mollitia velit, adipisci error illo a dolorem perspiciatis explicabo quis aliquam inventore neque reiciendis tempore, eius praesentium tempora, in ipsum labore? Totam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos aperiam quisquam exercitationem culpa. Harum, deserunt ipsam unde assumenda omnis illo nostrum necessitatibus nulla aspernatur eum non vitae? Fugit, eum explicabo.</p></div>
+</div>
+<div class="pe"></div>
+</body>
+</html>
